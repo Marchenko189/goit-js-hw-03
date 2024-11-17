4548f174f5c5f8b7898df56015e95bad3b715c47
Marchenko189 <marcenkoo189@gmail.com>
2024-11-17 19:04:18 +0200

    h

--- js/task-3.js
+++ js/task-3.js
@@ -1,16 +1,16 @@
-function checkForSpam(message) {
+// function checkForSpam(message) {
 
-// Слова в рядку параметра message можуть бути в довільному регістрі, наприклад SPAM або sAlE.
-    const banWord = message.toLowerCase();
+// // Слова в рядку параметра message можуть бути в довільному регістрі, наприклад SPAM або sAlE.
+//     const banWord = message.toLowerCase();
 
-// Якщо знайдено заборонене слово (spam або sale), то функція повертає буль true
-    return banWord.includes("spam") || banWord.includes("sale");
-}  
+// // Якщо знайдено заборонене слово (spam або sale), то функція повертає буль true
+//     return banWord.includes("spam") || banWord.includes("sale");
+// }  
 
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+// console.log(checkForSpam("Latest technology news")); // false
+// console.log(checkForSpam("JavaScript weekly newsletter")); // false
+// console.log(checkForSpam("Get best sale offers now!")); // true
+// console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+// console.log(checkForSpam("Trust me, this is not a spam message")); // true
+// console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+// console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
